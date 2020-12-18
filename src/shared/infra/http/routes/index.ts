3f550cff83bd6f6e4660a9b7d4da8d16c012b08b
Davi <davicpinheiro123@hotmail.com>
2020-12-18 18:15:10 -0300

    MailProvider

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,12 +2,14 @@ import { Router } from 'express';
 
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
+import passwordRouter from '@modules/users/infra/http/routes/password.routes';
 import animesRouter from '../../../../modules/animes/infra/http/routes/animes.routes';
 
 const router = Router();
 
 router.use('/animes', animesRouter);
 router.use('/users', usersRouter);
+router.use('/password', passwordRouter);
 router.use('/sessions', sessionsRouter);
 
 export default router;
